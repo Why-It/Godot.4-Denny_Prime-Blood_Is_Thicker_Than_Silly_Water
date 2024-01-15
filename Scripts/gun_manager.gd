ef271e445ae393afec5e38c5b240d40923a4009e
WyattC <60483679+Why-It@users.noreply.github.com>
2024-01-15 16:06:15 -0500

    -- Changes -- Added Sprite sheets by https://itch.io/profile/kodani for testing purposes Removed uneeded textures Added animations for the shotguns to fire, activate and deactivate Added logic to the weapon manager to switch between weapons Edited the parameters inside 'Shotgun_Resource' Edited 'Level00' with the new sprite tilemap

--- Scripts/gun_manager.gd
+++ Scripts/gun_manager.gd
@@ -1,64 +1,69 @@
 extends Node2D
 
-@export var animation_player = Node
+@export var anim_player : AnimationPlayer
 
 var cur_weapon = null
+
+var next_weapon : String
+var prev_weapon : String
+
 var weapon_stack = [] #An array of weapons being held by the player
 var weapon_indicator = 0
-var next_weapon : String
-var weapon_list = {}
+
+var weapon_list = {} #Dictionary of all weapons available to use
 
 @export var _weapon_resources : Array[shotgun_resource]
-
-@export var start_weapons : Array[String]
+@export var starting_arsenal : Array[String]
 
 func _ready():
-	#Initilize(start_weapons) #enter state machine
-	pass
-
+	Initilize(starting_arsenal) #Enter the state machine
+	
 func _input(event):
-	pass
-	#if event.is_action_pressed("Switch Next Weapon"):
-	#	weapon_indicator = min(weapon_indicator+1, weapon_stack.size()-1)
-	#	exit(weapon_stack[weapon_indicator])
+	if event.is_action_released("Weapon_Next"):
+		if weapon_indicator == weapon_stack.size()-1: # Roll over to the begginning of the array
+			weapon_indicator = weapon_stack.size()-weapon_stack.size()
+			exit(weapon_stack[weapon_indicator])
+		else:
+			weapon_indicator = min(weapon_indicator+1, weapon_stack.size()-1)
+			exit(weapon_stack[weapon_indicator])
+	
+	if event.is_action_released("Weapon_Prev"):
+		if weapon_indicator == weapon_stack.size()-weapon_stack.size(): # Roll over to the end of the array
+			weapon_indicator = weapon_stack.size()-1
+			exit(weapon_stack[weapon_indicator])
+		else:
+			weapon_indicator = max(weapon_indicator-1, 0)
+			exit(weapon_stack[weapon_indicator])
 		
-	#if event.is_action_pressed("Switch Previous Weapon"):
-	#	weapon_indicator = max(weapon_indicator-1, 0)
-	#	exit(weapon_stack[weapon_indicator])
-
-func Initilize(_start_weapons: Array):
-	pass
-	#for weapon in _weapon_resources:
-	#	weapon_list[weapon.weapon_name] = weapon
-		
-	#for i in _start_weapons:
-	#	weapon_stack.push_back(i) #Add out start weapon
 	
-	#cur_weapon = weapon_list[weapon_stack[0]] #set first item in the stack as the current weapon
-	#enter()
+func Initilize(_starting_arsenal : Array):
 	
-func enter():
-	pass
-	#Call when first entering into a weapon#
-	#animation_player.queue(cur_weapon.activate_anim)
+	#creating the dictionary
+	for weapon in _weapon_resources:
+		weapon_list[weapon.weapon_name] = weapon
 	
-func exit(_next_weapon: String):
-	pass
-	#in order to change weapons first call exit#
-	#if _next_weapon != cur_weapon.weapon_name:
-	#	if animation_player.get_current_animation() != cur_weapon.deactivate_anim:
-	#		animation_player.play(cur_weapon.deactivate_anim)
-	#		next_weapon = _next_weapon
+	for i in _starting_arsenal:
+		weapon_stack.push_back(i) #Add out start weapons
 	
-func Change_Weapon(weapon_name: String):
-	#cur_weapon = weapon_list[weapon_name]
-	#next_weapon = ""
-	#enter()
-	pass
+	cur_weapon = weapon_list[weapon_stack[0]]
+	enter()
 	
+func enter(): #Call when entering the next weapon
+	print(weapon_stack)
+	anim_player.queue(cur_weapon.activate_anim)
+	
+func exit(_next_weapon : String): #Call before changing weapons
+	if _next_weapon != cur_weapon.weapon_name:
+		if anim_player.get_current_animation() != cur_weapon.deactivate_anim:
+			anim_player.play(cur_weapon.deactivate_anim)
+			next_weapon = _next_weapon
+	
+func Change_Weapon(weapon_name : String):
+	cur_weapon = weapon_list[weapon_name]
+	next_weapon = ""
+	enter()
 
 
 func _on_animation_player_animation_finished(anim_name):
-	#if anim_name == cur_weapon.deactivate_anim:
-	#	Change_Weapon(next_weapon)
-	pass
+	if anim_name == cur_weapon.deactivate_anim:
+		Change_Weapon(next_weapon)
--- Scripts/shotgun_resource.gd
+++ Scripts/shotgun_resource.gd
@@ -4,16 +4,16 @@ class_name shotgun_resource
 
 @export var weapon_name : String
 @export var activate_anim : String
-@export var shoot_anim : String
-@export var cycle_anim : String
-@export var reload_anim : String
-@export var reload_anim_alt : String
 @export var deactivate_anim : String
-@export var out_of_ammo_anim : String
+@export var idle_anim : String
+@export var fire_anim : String
+@export var cycle_anim : String
+@export var ooa_anim : String
+@export var reload_anim : String
 
-@export var cur_ammo : int
+
+@export var loaded_ammo : int
 @export var reserve_ammo : int
-@export var magazine : int
 @export var max_ammo : int
 
 @export var auto_fire : bool
